b701a6323720941f5b56935adc86b2485fd07e26
iswayanabisma@gmail.com <BismaAjil@email.com>
2019-09-05 19:46:00 +0700

    Tebak Angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -2,35 +2,56 @@ package id.ac.polinema.tebakangka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText number_input;
+	private Button guess,reset;
+	int random = 0;
 
+	@SuppressLint("WrongViewCast")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		number_input = findViewById(R.id.number_input);
+		guess = findViewById(R.id.guess_button);
+		reset = findViewById(R.id.reset_button);
+		initRandomNumber();
+
 	}
 
 	// TODO: generate angka random di sini
-	private void initRandomNumber() {
+
+	private int initRandomNumber() {
+	random = (int)(Math.random()*100);
+	return random;
 
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		int number = Integer.parseInt(String.valueOf(number_input.getText()));
+		if (number > random) {
+			Toast.makeText(this, "Tebakan Anda Terlalu Besar", Toast.LENGTH_LONG).show();
+		}else if (number < random) {
+			Toast.makeText(this,"Tebakan Anda Terlalu Kecil", Toast.LENGTH_LONG).show();
+		}else {
+			Toast.makeText(this,"Tebakan Anda Terlalu Benar", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		number_input.setText("");
+		initRandomNumber();
 	}
 }
